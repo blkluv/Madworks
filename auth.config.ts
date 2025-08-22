069f898ee7fed5073cef0905482ef7e52631458a
breezynotcheezy <87101443+breezynotcheezy@users.noreply.github.com>
2025-08-22 13:43:29 -0400

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,18 @@ const authConfig = {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: 'https://www.madworksai.com/api/auth/callback/google'
+        }
+      }
     }),
   ],
   session: { strategy: "jwt" },
   pages: {
     signIn: "/login",
   },
+  trustHost: true,
 } satisfies NextAuthConfig
 
 export default authConfig
